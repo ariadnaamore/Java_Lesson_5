169a6e35ed6addb71968123474fb4147753fbf5e
ARIADNA <rodionova.iryna.b@gmail.com>
2020-09-07 13:26:50 +0300

    Rework #2

--- Javadeveloper/JavaCore3/src/jc3HomeRework/Circle.java
+++ Javadeveloper/JavaCore3/src/jc3HomeRework/Circle.java
@@ -7,15 +7,21 @@ public class Circle {
 		this.rad = 15;
 	}
 
-	public double squareR(){
+	public double squareR() {
 
-		return Math.PI*rad*rad;
-		}
-	
+		return Math.PI * rad * rad;
+	}
+
 	public double perimetrR() {
-		return 2*rad*Math.PI;
-		
+		return 2 * rad * Math.PI;
+
+	}
+	public double perimetrR2(int rad) {
+		return 2 * rad * Math.PI;
+
+	}
+	public double squareR2(int rad) {
+
+		return Math.PI * rad * rad;
 	}
 }
-
-
--- Javadeveloper/JavaCore3/src/jc3HomeRework/RectangleR.java
+++ Javadeveloper/JavaCore3/src/jc3HomeRework/RectangleR.java
@@ -1,34 +1,50 @@
 package jc3HomeRework;
 
 public class RectangleR {
+
+	public int longitude;
+	public int width;
+
+	RectangleR() {
+		this.longitude = 45;
+		this.width = 79;
+
+	}
+
+	public int recsquare(int longitude, int width) {
+
+		return width * longitude;
+	}
+
+	public int recsquare2() {
+
+		return width * longitude;
+	}
+
+	public int recperim(int longitude, int width) {
+		return (longitude + width) * 2;
+	}
+
 	
-	private	int longitude;
-	private	int width;
 
-		RectangleR() {
-			this.longitude = 45;
-			this.width = 79;
+	public int recperim2() {
+		return (longitude + width) * 2;
+	}
 
-		}
-		public int recsquare() {
+	public int getLongitude() {
+		return longitude;
+	}
 
-			return  width * longitude;
-		}
-		
-		public int recperim() {
-			return (longitude + width) * 2;
-		}
-		public int getLongitude() {
-			return longitude;
-		}
-		public void setLongitude(int longitude) {
-			this.longitude = longitude;
-		}
-		public int getWidth() {
-			return width;
-		}
-		public void setWidth(int width) {
-			this.width = width;
-		}
-		
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
 }
