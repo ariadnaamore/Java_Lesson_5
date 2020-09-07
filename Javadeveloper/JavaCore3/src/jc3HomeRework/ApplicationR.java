681f605d0525dffd4eeafbeae9a0b65eecf237d4
ARIADNA <rodionova.iryna.b@gmail.com>
2020-09-07 13:27:55 +0300

    rework

--- Javadeveloper/JavaCore3/src/jc3HomeRework/ApplicationR.java
+++ Javadeveloper/JavaCore3/src/jc3HomeRework/ApplicationR.java
@@ -10,27 +10,26 @@ public class ApplicationR {
 		rectan1.setLongitude(20);
 		rectan3.setWidth(25);
 		rectan3.setLongitude(40);
-		
 
 		System.out.println("Довжина прямокутника #1 = " + rectan1.getLongitude());
 		System.out.println("Ширина прямокутника #1 = " + rectan1.getWidth());
 		System.out.println("Периметр прямокутника #1 = " + rectan1.recperim2());
-           System.out.println("Площа прямокутника #1 = " + rectan1.recsquare2());
+		System.out.println("Площа прямокутника #1 = " + rectan1.recsquare2());
 
 		System.out.println("Довжина прямокутника по замовчуванню = " + rectan2.getLongitude());
 		System.out.println("Ширина прямокутника по замовчуванню = " + rectan2.getWidth());
 		System.out.println("Периметр прямокутника #2 = " + rectan2.recperim2());
-	System.out.println("Площа прямокутника #2 = " + rectan2.recsquare2());
-	
-	System.out.println("Довжина прямокутника #3 = " + rectan3.getLongitude());
-	System.out.println("Ширина прямокутника #3 = " + rectan3.getWidth());
+		System.out.println("Площа прямокутника #2 = " + rectan2.recsquare2());
 
-System.out.println("Площа прямокутника #3 = " + rectan2.recsquare(rectan3.longitude, rectan3.width));
-	System.out.println("Периметр прямокутника #3 = " + rectan3.recperim(rectan3.longitude, rectan3.width));
+		System.out.println("Довжина прямокутника #3 = " + rectan3.getLongitude());
+		System.out.println("Ширина прямокутника #3 = " + rectan3.getWidth());
+
+		System.out.println("Площа прямокутника #3 = " + rectan2.recsquare(rectan3.longitude, rectan3.width));
+		System.out.println("Периметр прямокутника #3 = " + rectan3.recperim(rectan3.longitude, rectan3.width));
 
 		System.out.println("Периметр кола = " + circl.perimetrR());
 		System.out.println("Полоща кола = " + circl.squareR());
-		System.out.println("Довжина кола з радіусом "+circl.perimetrR2(45));
+		System.out.println("Довжина кола з радіусом " + circl.perimetrR2(45));
 	}
 
 }
